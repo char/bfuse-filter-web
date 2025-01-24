df8c98705b1a84a46ee3c9c71e12faf5b8de0f80
Charlotte Som <charlotte@som.codes>
2025-01-24 09:18:41 +0000

    fix char.css deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 deno task build
 cp -r web/* dist/public/
+unlink dist/public/char.css && cp ./vendor/char.css/char.css dist/public/char.css
 (cd dist && git add -A . && git commit -m "$(date)" && git push)
