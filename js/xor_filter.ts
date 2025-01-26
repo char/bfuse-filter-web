4f362bd71206fc2bbc7a9eb13b9bf008ef84efcb
Charlotte Som <charlotte@som.codes>
2025-01-26 09:29:55 +0000

    drop deno.json imports entry for xor-filter

--- js/xor_filter.ts
+++ js/xor_filter.ts
@@ -1,6 +1,6 @@
-import init, { check, generate } from "xor-filter/xor_filter.js";
+import init, { check, generate } from "../rust/pkg/xor_filter.js";
 // @ts-types="./xor_filter_wasm.d.ts"
-import wasmURL from "xor-filter/xor_filter_bg.wasm";
+import wasmURL from "../rust/pkg/xor_filter_bg.wasm";
 await init(new URL(wasmURL, import.meta.url));
 
 export { check, generate };
